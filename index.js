5739ecce5a69c1a76587edddb39d9b4b0d500b94
mridulgupta2906 <mridulgupta290@gmail.com>
2021-03-13 16:42:52 +0530

    added cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app=express();
 const bodyparser=require('body-parser')
 const route=require('./routes')
 const port=process.env.PORT || 4000;
+var cors = require('cors')
 
 app.use(bodyparser.urlencoded({extended:'true',limit:'50mb'}))
 app.use(bodyparser.json({limit:'50mb'}))
-
+app.use(cors());
 app.use('/',route);
 
 app.listen(port,()=>{
